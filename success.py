05034add5ecdea49603a47aaa471bf29ddd45bcb
Sahilcode2704 <sahiln27042008@gmail.com>
2025-07-26 00:14:21 +0530

    update success.py

--- success.py
+++ success.py
@@ -1,1 +1,21 @@
 print("I am successful already,hahaha")
+
+import numpy as np
+
+# Create two arrays
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+# Element-wise addition
+c = a + b
+
+# Print results
+print("a:", a)
+print("b:", b)
+print("a + b:", c)
+
+print("Lord HANUMAN JI ARE THE GREATEST")
+
+
+
+
