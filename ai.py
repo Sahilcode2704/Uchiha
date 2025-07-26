76360c17fbb1fa27f12f35906a8e056979fece25
Sahilcode2704 <sahiln27042008@gmail.com>
2025-07-26 14:20:45 +0530

    update ai.py

--- ai.py
+++ ai.py
@@ -3,6 +3,7 @@ from google import genai
 client = genai.Client(api_key="AIzaSyCqQIbb7XP-aJ-E-sThLLdKB4SJlW1tCB0")
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="5858*62736"
+    model="gemini-2.5-flash", contents="who are you"
 )
+print(response)
 print(response.text)
