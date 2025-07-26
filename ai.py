028b794816c6ac0fe3077026b32afee7206335e2
Sahilcode2704 <sahiln27042008@gmail.com>
2025-07-26 14:31:34 +0530

    update ai.py

--- ai.py
+++ ai.py
@@ -2,17 +2,14 @@ from google import genai
 
 client = genai.Client(api_key="AIzaSyCqQIbb7XP-aJ-E-sThLLdKB4SJlW1tCB0")
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="who are you"
-)
-print(response)
-print(response.text)
+#response = client.models.generate_content(
+#    model="gemini-2.5-flash", contents="who are you")
+#print(response.text)
 
 def send(): 
   content = input("Enter your question")
   response = client.models.generate_content(
   model="gemini-2.5-flash", contents=content)
-  print(response)
   print(response.text)
 
 while True: 
