65ef06933429726fbfd3bd18cea36337f7ea57a6
Sahilcode2704 <sahiln27042008@gmail.com>
2025-07-26 14:18:37 +0530

    update ai.py

--- ai.py
+++ ai.py
@@ -3,6 +3,6 @@ from google import genai
 client = genai.Client(api_key="AIzaSyCqQIbb7XP-aJ-E-sThLLdKB4SJlW1tCB0")
 
 response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Who are NARENDRA MODI"
+    model="gemini-2.5-flash", contents="5858*62736"
 )
 print(response.text)
