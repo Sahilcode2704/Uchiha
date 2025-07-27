4f4e3708629046df1461bf2eda433f5ace4f2f53
Sahilcode2704 <sahiln27042008@gmail.com>
2025-07-27 20:45:10 +0530

    update email.py

--- email.py
+++ email.py
@@ -2,26 +2,32 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-# Configuration
-sender_email = "sahiln27042008@gmail.com"
-receiver_email = input("Enter the recipient email-")
-app_password = "feaylvqzmrfzaoil"  # 16-char app password
-subject = "Check Email"
-body = "Hello you are gonna be really successful and rich. GOD PROMISE"
-
-# Compose email
-msg = MIMEMultipart()
-msg['From'] = sender_email
-msg['To'] = receiver_email
-msg['Subject'] = subject
-msg.attach(MIMEText(body, 'plain'))
-
-# Send email via Gmail SMTP
-try:
-    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+def email(ine,msgx): 
+    # Configuration
+    sender_email = "sahiln27042008@gmail.com"
+    receiver_email = ine
+    app_password = "feaylvqzmrfzaoil"  # 16-char app password
+    subject = "Check Email"
+    body = msg
+    # Compose email
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+    
+    # Send email via Gmail SMTP
+    try:
+        with smtplib.SMTP('smtp.gmail.com', 587) as server:
         server.starttls()
         server.login(sender_email, app_password)
         server.send_message(msg)
         print("✅ Email sent successfully!")
-except Exception as e:
-    print("❌ Error sending email:", e)
+        except Exception as e:
+            print("❌ Error sending email:", e)
+    
+
+email("sahiln27042008","Dhfhydhdveeih")
+    
+    
+    
