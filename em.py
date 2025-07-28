b85220f34476ce6adc1d6affd120655d0b8079be
Sahilcode2704 <sahiln27042008@gmail.com>
2025-07-28 13:55:41 +0530

    update em.py

--- em.py
+++ em.py
@@ -1,5 +1,11 @@
-urlp="https://sahilcode2704.github.io/Uchiha/contacts.csv"
-
 import pandas as pd
+import email as e
+
+urlp="https://sahilcode2704.github.io/Uchiha/contacts.csv"
 contacts = pd.read_csv(urlp)
 print(contacts)
+
+contacts.for
+
+
+
